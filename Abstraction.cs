dc4f12d264e0ecf1af095553d3b3f16558fa83c6
Mert Topcu <merttopcu.dev@gmail.com>
2024-08-03 12:56:41 +0300

    Add project files.

---
+++ Abstraction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaAbstractionTask
+{
+    // ▼ Base Class with abstract method for workers ▼
+    public abstract class BaseWorker
+    {
+        // ▼ Base Class properties for all worker ▼
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Department { get; set; }
+        public string Role { get; set; }
+
+        // ▼ Abstract method for workers ▼ 
+        public abstract void Title();
+    }
+
+    // ▼ Worker Baby Class ▼
+    public class Worker : BaseWorker 
+    {
+        // ▼ Constructor of worker with defining properties coming from base class ▼
+        public Worker(string name, string surname, string department,string role)
+        {
+            Name = name;
+            Surname = surname;
+            Department = department;
+            Role = role;
+        }
+        // ▼ Overriding abstract method coming from base class ▼
+        public override void Title()
+        {
+            Console.WriteLine($"{Name} {Surname} is here. I work in {Department} deparment as {Role}.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaAbstractionTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ▼ Instance for Worker with constructor defining props ▼
+            BaseWorker worker1 = new Worker("Mert","Topcu","Strategy","KPI & Performance Executive");
+
+            // ▼ Calling abstract method from base class that displaying worker1 role and department in console ▼
+            worker1.Title(); // -> "Mert Topcu is here. I work in Strategy deparment as KPI & Performance Executive".
+
+            // ▼ Instance for Worker with constructor defining props ▼
+            BaseWorker worker2 = new Worker("Sertan", "Bozkuş", "Learning", "Technical Coordinator");
+
+            // ▼ Calling abstract method from base class that displaying worker1 role and d"epartment in console ▼
+            worker2.Title(); // -> "Sertan Bozkuş is here. I work in Learning deparment as Technical Coordinator."
+
+        }
+    }
+}
